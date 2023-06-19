44a8cb7e0545fab1a2e0e8b48e364c74912f7165
TUSHAR RATHOD <118971581+TusharRathod10@users.noreply.github.com>
2023-06-19 16:42:34 +0530

    Add files via upload

--- ajax/login.php
+++ ajax/login.php
@@ -1,17 +1,21 @@
 <?php
 
 include('../config/db.php');
-
 if (!empty($_SESSION['admin'])) {
-    header('location:../html/index.php');
-}
+    if ($_SESSION['admin']['role'] == 'admin') {
+      header('location:../html/index.php');
+    } else {
+      header('location:../online-shop-website-template/index.php');
+    }
+  }
 
-$email = $_POST['email'];
+$email = $_POST['email_number'];
+$number = $_POST['email_number'];
 $password = $_POST['password'];
 $response = [];
 
 if (empty($email)) {
-    $response['email_err'] = "Email is required.";
+    $response['email_number_err'] = "Email or Number is required.";
     $response['status'] = 400;
 }
 
@@ -21,7 +25,7 @@ if (empty($password)) {
 }
 if (empty($response)) {
 
-    $login_data = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM register WHERE `email`='$email' AND `password`='$password'"));
+    $login_data = mysqli_fetch_assoc(mysqli_query($con, "SELECT * FROM register WHERE (`email`='$email' OR `phone_no` = '$number') AND `password`='$password'"));
 
     if ($login_data) {
         $_SESSION['admin'] = $login_data;
@@ -29,7 +33,7 @@ if (empty($response)) {
         $response['success'] = "Login successfully.";
         $response['status'] = 200;
     } else {
-        $response['error'] = "Invalid email and password !";
+        $response['error'] = "Invalid email/number and password !";
         $response['status'] = 400;
     }
 } else {
--- ajax/register.php
+++ ajax/register.php
@@ -1,11 +1,19 @@
 <?php
 
 include('../config/db.php');
-
+if (!empty($_SESSION['admin'])) {
+    if ($_SESSION['admin']['role'] == 'admin') {
+      header('location:../html/index.php');
+    } else {
+      header('location:../online-shop-website-template/index.php');
+    }
+  }
 $username = $_POST['username'];
 $email = $_POST['email'];
+$number = $_POST['number'];
 $password = $_POST['password'];
 $response = [];
+
 if (empty($username)) {
     $response['username_err'] = "Username is required.";
     $response['status'] = 400;
@@ -16,22 +24,38 @@ if (empty($email)) {
     $response['status'] = 400;
 }
 
+if (empty($number)) {
+    $response['number_err'] = "Number is required.";
+    $response['status'] = 400;
+}
+
 if (empty($password)) {
     $response['password_err'] = "Password is required.";
     $response['status'] = 400;
 }
 
+$no_length = strlen($number);
+if (!empty($number) && ($no_length < 10)) {
+    $response['length_err'] = "Number Length must be 10 !";
+    $response['status'] = 400;
+}
+
 if (empty($response)) {
-    $user = "SELECT email FROM register WHERE `email`='$email'";
-    $user_exe = mysqli_query($con, $user);
-    $user_count = mysqli_num_rows($user_exe);
+    $user_email = mysqli_query($con, "SELECT email FROM register WHERE `email`='$email'");
+    $user_count = mysqli_num_rows($user_email);
+
+    $user_number = mysqli_query($con, "SELECT phone_no FROM register WHERE `phone_no`='$number'");
+    $num_count = mysqli_num_rows($user_number);
 
     if ($user_count > 0) {
         $response['repeat'] = "Email Already Exists !";
         $response['status'] = 400;
+    } elseif ($num_count > 0) {
+        $response['no_repeat'] = "Number Already Exists !";
+        $response['status'] = 400;
     } else {
 
-        $insert = "INSERT INTO register (`username`,`email`,`password`) VALUES ('$username','$email','$password')";
+        $insert = "INSERT INTO register (`username`,`email`,`phone_no`,`password`,`role`) VALUES ('$username','$email','$number','$password','user')";
         $insert_exe = mysqli_query($con, $insert);
         $response['success'] = "Register successfully.";
         $response['status'] = 200;
